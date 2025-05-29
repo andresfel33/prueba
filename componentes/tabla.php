6636489e656a16cbf402af8fd4f842cf8a0cba65
andresfel33 <m36890242@gmail.com>
2025-05-29 10:29:53 -0500

    Update tabla.php
    se adiciona el filtro por email y nombre de la tabla contactos tambien se ubica mejor el titulo contactos y su boton añadir y el a href cerrar sesion

--- componentes/tabla.php
+++ componentes/tabla.php
@@ -4,12 +4,26 @@ $conn=conexion();
 ?>
 <div class="row">
 	<div class="col-sm-12">
-		<h2>Contactos</h2>
-		<p><caption>
+		
+<div class="container mt-4">
+	 <form oninput="filtrarTabla()">
+		<h5 class="text-center mb-4 text-primary">Filtrar Contactos</h5>
+		 <div class="mb-3">
+            <label for="Nombre">Nombre:</label>
+            <input class="form-control" type="text" id="name" name="name">
+		</div>
+		<div class="mb-3">
+			<label for="email">Email:</label>
+			<input class="form-control" type="text" id="correo" name="correo">
+		</div>
+        </form>
+</div>
+<h2 class="text-center text-primary">Contactos</h2>
+<caption>
 				<button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modalregistro"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
   <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
 </svg> &nbsp; Añadir</button>
-			</caption></p>
+			</caption><a href="cerrarsesion.php">cerrar sesion</a>
 		<table class="table table-hover table-condensed table-bordered">
 			
 			<tr>
